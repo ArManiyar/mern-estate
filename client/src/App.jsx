acd154263c4e6da79bc371ea771305d48eba1da3
ArManiyar <arsufiyan.maniyar@gmail.com>
2023-12-04 23:30:51 +0530

    Installed tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-slate-900">Hello world!</h1>
+    </div>
+  );
 }
