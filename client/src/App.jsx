8915f47d15d16675788e950e0a3d2b23e3b318c8
ArManiyar <arsufiyan.maniyar@gmail.com>
2023-12-05 00:15:58 +0530

    Commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignOut from "./pages/SignOut";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-slate-900">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
