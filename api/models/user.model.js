ba719d698c8a9f3fd8916b57ac8b8ccb1b77879d
AbuSufiyan Maniyar <86867746+ArManiyar@users.noreply.github.com>
2023-12-05 13:01:56 +0530

    Build header and user model file str fix (#2)
    * Added user model and db con
    
    * file str fix

---
+++ api/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timeStamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
